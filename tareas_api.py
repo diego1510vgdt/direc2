2e05639fe0a1f6a9d2054a1d8a71910465f31b0c
diego1510vgdt <diego1510vgdt@outlook.com>
2021-03-03 05:26:47 -0600

    Se agregó la seguridad al programa de apis, Se corrigió la versión anterior con la vista en clase

--- tareas_api.py
+++ tareas_api.py
@@ -1,69 +1,88 @@
-from flask import Flask, jsonify, abort, request
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import db
+import os
+from flask import Flask, request, jsonify, abort
+from firebase_admin import credentials, db, initialize_app
+
+#Inicializar la app con Flask
 
 app = Flask(__name__)
 
-cred = credentials.Certificate("firebase_adminsdk.json")
-default_app = firebase_admin.initialize_app(cred,{
+cred = credentials.Certificate('firebase_adminsdk.json')
+default_app = initialize_app(cred,{
     'databaseURL':'https://test-api-374a2-default-rtdb.firebaseio.com/'
 })
 
-ref = db.reference('/')
+ref = db.reference('tasks')
 
-@app.route('/')
-def hello_world():
-    return 'Bienvenido! API To-Do List'
-
-@app.route('/api/tasks', methods=['GET'])
-def get_tasks():
-    return ref.get()
-
-@app.route('/api/tasks/<int:id>', methods=['GET'])
-def get_task(id):
-    if(ref.child("tasks").child(str(id)).get()==None):
-        abort(404)
-    return jsonify(ref.child("tasks").child(str(id)).get())
-
-@app.route('/api/tasks', methods = ['POST'])
-def create_task():
-    if not request.json:
-        abort(404)
-    id = len(ref.child("tasks").get())
-    ref.child("tasks").child(str(id)).set({
-        "check": False,
-        "name": request.json['name']
+"""
+@app.route('/', methods=['POST'])
+def setting():
+    ref.set({
+        'tasks': {
+            '1':{
+                'id':'1',
+                'name':'Primer tarea',
+                'check':False
+            }
+        }
     })
-    return jsonify(ref.child("tasks").child(str(id)).get()), 201
+"""
 
-@app.route('/api/tasks/<int:task_id>', methods = ['PUT'])
-def update_task(task_id):
-    if ref.child("tasks").child(str(task_id)).get()==None:
-        abort(404)
-    if not request.json:
-        abort(400)
-    """
-    if 'name' in request.json and type(request.json['name']) is not str:
-        abort(400)
-    if 'check' in request.json and type(request.json['check']) is not bool:
-        abort(400)
-    """
-    ref.child("tasks").child(str(task_id)).update({
-        "name": request.json.get('name', ref.child("tasks").child(str(task_id)).child("name").get),
-        "check": request.json.get('check', ref.child("tasks").child(str(task_id)).child("check").get)
-    })
-    
-    return jsonify(ref.child("tasks").child(str(task_id)).get())
+@app.route('/read', methods=['GET'])
+def read():
+    try:
+        id = request.args.get('id')
+        if id:
+            return jsonify(ref.child(id).get()), 200
+        else: 
+            return jsonify(ref.get()), 200
+    except Exception as e:
+        return f"Ocurrio el siguiente error: {e}"
 
-@app.route('/api/tasks/<int:task_id>', methods=['DELETE'])
-def delete_task(task_id):
-    if ref.child("tasks").child(str(task_id)).get()==None:
-        abort(404)
-    ref.child("tasks").child(str(task_id)).delete()
-    return jsonify({'result': True})
+@app.route('/add', methods=['POST'])
+def create():
+    try:
+        if not request.json:
+            abort(404)
+        id = str(len(ref.get()))
+        task = {
+            'id': id,
+            'name': request.json['name'],
+            'check': False
+        }
+        ref.child(id).set(task)
+        return jsonify({"succes": True}), 200
+    except Exception as e:
+        return f"Ocurrio el siguiente error: {e}"
 
 
+@app.route('/update', methods=['PUT'])
+def update():
+    try:
+        id = request.json['id']
+        if ref.child(id).get()==None:
+            abort(404)
+        if not request.json:
+            abort(400)
+        ref.child(id).update(request.json)
+        return jsonify({"succes": True}), 200
+    except Exception as e:
+        return f"Ocurrio el siguiente error: {e}"
+
+@app.route('/delete', methods=['DELETE'])
+def delete():
+    try:
+        id = request.args.get('id')
+        if ref.child(id).get()==None:
+            abort(404)
+        ref.child(id).delete()
+        return jsonify({"succes": True}), 200
+    except Exception as e:
+        return f"Ocurrio el siguiente error: {e}"
+
+port = int(os.environ.get('PORT',8000))
 if __name__ == '__main__':
+    app.run(threaded=True, host='0.0.0.0', port=port)
     app.run(debug=True)
 
+#set FLASK_APP=app.py
+#flask run
